f3af6df688205847f713a58d1e9e1e828b785ea5
Anna Vasilchenko <annavasilcenko482@gmail.com>
2023-02-07 16:47:36 +0300

    Добавляет программу расчета суммы элементов на нечетных позициях

--- sum_numbers.py
+++
@@ -1,8 +0,0 @@
-# Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
-number_current = input('Введите число: ').replace('.', '0')
-digit_list = list(number_current)
-result = 0
-for i in digit_list:
-    i = int(i)
-    result += i
-print(result)
