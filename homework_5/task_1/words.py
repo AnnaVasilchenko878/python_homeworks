a7855fcfd7d20681aa1933a3d6bdd6e682153a7d
Anna Vasilchenko <annavasilcenko482@gmail.com>
2023-02-25 14:49:49 +0300

    Исправляет ошибки в первой задаче

--- homework_5/task_1/words.py
+++ homework_5/task_1/words.py
@@ -1,13 +1,5 @@
 # Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 # Входные и выходные данные хранятся в отдельных текстовых файлах.
-
-def del_text(current_string):
-    words = 'aбв'
-    new_string = current_string.lower().replace(words, '')
-    new_string = new_string[0].upper()+new_string[1:]
-    return new_string
-
-
 def get_string():
     path = 'words_input.txt'
     data = open(path, 'r', encoding='utf-8')
@@ -15,6 +7,16 @@ def get_string():
         current_string = line
     data.close()
     return current_string
+
+
+def del_words(current_string):
+    words = current_string.split(' ')
+    temp = []
+    for word in words:
+        if ('Абв' or 'абв') not in word:
+            temp.append(word)
+    new_string = ' '.join(temp)
+    return new_string
 
 
 def write_result(result):
@@ -25,5 +27,5 @@ def write_result(result):
 
 
 current_string = get_string()
-new_string = del_text(current_string)
+new_string = del_words(current_string)
 write_result(new_string)
