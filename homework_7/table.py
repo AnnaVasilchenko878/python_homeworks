aef3d370d5a265a3bbbcd7bc3f8bcfd7bfd610ca
Anna Vasilchenko <annavasilcenko482@gmail.com>
2023-02-26 17:55:10 +0300

    Добавляет второе задание

--- homework_7/table.py
+++ homework_7/table.py
@@ -7,3 +7,18 @@
 # 4 8 12 16 20 24
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
+
+
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for y in range(1, num_rows+1):
+        for x in range(1, num_columns+1):
+            print(operation(x, y), end=' ')
+        print()
+
+
+operation = lambda x, y: x*y
+operation_sum = lambda x,y: x+y
+
+
+print_operation_table(operation)
+print_operation_table(operation_sum)
